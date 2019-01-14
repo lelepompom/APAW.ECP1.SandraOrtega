3632941d5848779fabd621891022661e3a26ab31
Sandra Ortega <sandrita.saan@gmail.com>
2019-01-14 12:37:04 +0100

    Book Class and Test

---
+++ src/test/java/BookTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BookTest {
+    private Book book;
+
+    @BeforeEach
+    void before(){
+        this.book = new Book("Design as art", "Bruno Munari");
+    }
+
+    @Test
+    void testTitle(){
+        assertEquals("Design as art", book.getTitle());
+    }
+
+    @Test
+    void testAuthor() {
+        assertEquals("Bruno Munari", book.getAuthor());
+    }
+}
