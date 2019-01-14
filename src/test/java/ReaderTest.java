556235627af54c6fd1a13c862e3adf8f70ec9536
Sandra Ortega <sandrita.saan@gmail.com>
2019-01-14 12:58:16 +0100

    Reader Class and Test

---
+++ src/test/java/ReaderTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ReaderTest {
+    private Reader reader;
+
+    @BeforeEach
+    void before() {
+        this.reader = new Reader("Almudena", 25);
+    }
+
+    @Test
+    void testName() {
+        assertEquals("Almudena", reader.getName());
+    }
+
+    @Test
+    void testAge() {
+        assertEquals("25", reader.getAge().toString());
+    }
+
+}
